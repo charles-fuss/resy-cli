8ee608fcf00ad6b66a30a1d251eb370a92cf047f
Charles Fuss <charlesfuss8@gmail.com>
2025-08-30 15:49:27 -0400

    asd

--- step.ps1
+++ step.ps1
@@ -23,6 +23,6 @@
                 echo book  --partySize=$env:PARTY_SIZE      --reservationDate=$date   --reservationTimes=$t   --venueId=$env:VENUE_ID --reservationTypes=""
 
                 Write-Host "Attempting booking at time: $t"
-                .\resy-cli-windows-amd64.exe book --partySize=$env:PARTY_SIZE  --reservationDate=$date --reservationTimes=$t --venueId=$env:VENUE_ID --reservationTypes=""              }
+                sudo .\resy-cli-windows-amd64.exe book --partySize=$env:PARTY_SIZE  --reservationDate=$date --reservationTimes=$t --venueId=$env:VENUE_ID --reservationTypes=""              }
 
               Write-Host "All attempts finished."
